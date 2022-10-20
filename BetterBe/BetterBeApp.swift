dda76f32bb5992f950025b12d8b4822b585bef47
John Critchlow <80559126+johncritch@users.noreply.github.com>
2022-10-20 15:04:26 -0600

    Initial Commit

---
+++ BetterBe/BetterBeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterBeApp.swift
+//  BetterBe
+//
+//  Created by John Critchlow on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterBeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
